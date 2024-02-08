7d049d9e76c508248cda76d8742e4bff359f970e
ethanc8 <echaroenpitaks@gmail.com>
2024-02-07 23:44:30 -0600

    Redo BasicTableViewPlayground in ProjectCenter

---
+++ BasicTableViewPlayground/AppController.h
@@ -0,0 +1,39 @@
+/* 
+   Project: BasicTableViewPlayground
+
+   Author: Ethan Charoenpitaks
+
+   Created: 2024-02-07 23:42:03 -0600 by ethan
+   
+   Application Controller
+*/
+ 
+#ifndef _PCAPPPROJ_APPCONTROLLER_H
+#define _PCAPPPROJ_APPCONTROLLER_H
+
+#import <AppKit/AppKit.h>
+// Uncomment if your application is Renaissance-based
+//#import <Renaissance/Renaissance.h>
+
+@interface AppController : NSObject
+{
+}
+
++ (void)  initialize;
+
+- (id) init;
+- (void) dealloc;
+
+- (void) awakeFromNib;
+
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif;
+- (BOOL) applicationShouldTerminate: (id)sender;
+- (void) applicationWillTerminate: (NSNotification *)aNotif;
+- (BOOL) application: (NSApplication *)application
+	    openFile: (NSString *)fileName;
+
+- (void) showPrefPanel: (id)sender;
+
+@end
+
+#endif
---
+++ BasicTableViewPlayground/BasicTableViewPlayground_main.m
@@ -0,0 +1,30 @@
+/* 
+   Project: BasicTableViewPlayground
+
+   Author: Ethan Charoenpitaks
+
+   Created: 2024-02-07 23:42:03 -0600 by ethan
+*/
+
+#import <AppKit/AppKit.h>
+
+int 
+main(int argc, const char *argv[])
+{
+// Uncomment if your application is Renaissance application
+/*  CREATE_AUTORELEASE_POOL (pool);
+  [NSApplication sharedApplication];
+  [NSApp setDelegate: [AppController new]];
+
+  #ifdef GNUSTEP
+    [NSBundle loadGSMarkupNamed: @"MainMenu-GNUstep"  owner: [NSApp delegate]];
+  #else
+    [NSBundle loadGSMarkupNamed: @"MainMenu-OSX"  owner: [NSApp delegate]];
+  #endif
+   
+  RELEASE (pool);
+*/
+
+  return NSApplicationMain (argc, argv);
+}
+
